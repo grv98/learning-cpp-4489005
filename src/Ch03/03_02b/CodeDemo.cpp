7559bcea7a054ad1e42132a5a2e6ae26d98f9f1a
grv98 <42107330+grv98@users.noreply.github.com>
2024-08-08 18:13:26 +0000

    commit all

--- src/Ch03/03_02b/CodeDemo.cpp
+++ src/Ch03/03_02b/CodeDemo.cpp
@@ -7,20 +7,48 @@
 
 enum class cow_purpose {dairy, meat, hide, pet};
 
-struct cow{
+class cow{
+public:
+    cow(std::string name_i, int age_i, cow_purpose purpose_i){
+        name = name_i;
+        age = age_i;
+        purpose = purpose_i;
+    }
+    //getter
+    std::string getName() const{
+        return name;
+    }
+    int getAge() const{
+        return age;
+    }
+    cow_purpose getcowPurpose() const{
+        return purpose;
+    }
+    //setters
+    void setName(std::string newName){
+        name = newName;
+    }
+    void setAge(int newAge){
+        age = newAge;
+    }
+    void setPurpose(cow_purpose newPurpose){
+        purpose = newPurpose;
+    }
+private:
     std::string name;
     int age;
     cow_purpose purpose;
 };
 
 int main(){
-    cow my_cow;
-    my_cow.age = 5;
-    my_cow.name = "Betsy";
-    my_cow.purpose = cow_purpose::dairy;
-    std::cout << my_cow.name << " is a type-" << (int)my_cow.purpose << " cow." << std::endl;
-    std::cout << my_cow.name << " is " << my_cow.age << " years old." << std::endl;
+    cow my_cow("kamdhenu",5,cow_purpose::dairy);
+    std::cout << my_cow.getName() << " is a type-" << (int)my_cow.getcowPurpose() << " cow." << std::endl;
+    std::cout << my_cow.getName() << " is " << my_cow.getAge() << " years old." << std::endl;
     
     std::cout << std::endl << std::endl;
+    
+    my_cow.setAge(10);
+
+    std::cout << my_cow.getName() << " is " << my_cow.getAge() << " years old." << std::endl;
     return (0);
 }
